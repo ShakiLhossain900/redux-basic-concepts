1d3e144e6bb7a7a1986c7ec625e879b5e6463612
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-27 10:57:56 +0600

    https://github.com/ShakiLhossain900/core-Redux-Concepts

---
+++ redux-demo.js
@@ -0,0 +1,32 @@
+const redux = require("redux");
+
+//reducer
+const initialState ={
+  counter:0,
+}
+const counterReducer = (state, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = redux.createStore(counterReducer,initialState);
+
+//subscriber
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
