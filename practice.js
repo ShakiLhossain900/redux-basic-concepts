abb5116fca7393b8a582c71537c02681c39ef205
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-30 20:16:13 +0600

    adding practice

--- practice.js
+++ practice.js
@@ -1,37 +1,26 @@
-const redux = require("redux");
+const redux = requre("redux");
 
+//reducer
 const initialState = {
   counter: 0,
 };
 
 const counterReducer = (state, action) => {
   if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
+    return { counter: state.counter + 1 };
   }
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
+  if (action.type === "decrement"){
+    return { counter: state.counter - 1 };
   }
   return state;
 };
 
 const store = redux.createStore(counterReducer, initialState);
 
-const counterSubscriber = () => {
-  const latestState = store.getState();
-  console.log(latestState);
-};
+//subscriber
+const counterSubscriber = () =>{
+    const latestState = store.getState();
+    console.log(latestState);
+}
 
 store.subscribe(counterSubscriber);
-
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "increment" });
-
-store.dispatch({ type: "decrement" });
-store.dispatch({ type: "decrement" });
-store.dispatch({ type: "decrement" });
-store.dispatch({ type: "decrement" });
